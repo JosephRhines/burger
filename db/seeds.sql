9fc598cec0d3655d5259e82b63818085ace86bc9
Joseph Rhines <jprhines@gmail.com>
2021-02-10 18:09:38 -0600

    I added the values to the table by clicking the lighting bolt and also added semi colon

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (name) VALUES ('Big Mac')
-INSERT INTO burgers (name) VALUES ('Whopper')
-INSERT INTO burgers (name) VALUES ('Double Stack')
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name) VALUES ('Double Stack');
 INSERT INTO burgers (name) VALUES ('Whopper Jr')
